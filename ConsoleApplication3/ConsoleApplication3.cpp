4a43092259a08936111a022454b9263c26ed03fb
manelisi <manelisihlazo2@gmail.com>
2026-03-12 11:10:46 +0200

    Add files via upload

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int temp1;
+    int temp2;
+    int temp3;
+    int difference;
+
+    // Input first temperature
+    cout << "Enter the first temperature reading: ";
+    cin >> temp1;
+
+    // Input second temperature
+    cout << "Enter the second temperature reading: ";
+    cin >> temp2;
+
+    // Calculate difference
+    difference = temp2 - temp1;
+
+    // Check temperature increase conditions
+    if (difference > 50) {
+        cout << "Reduce fryer heat before taking the third reading" << endl;
+    }
+    else if (difference < 10) {
+        cout << "Increase the fryer heat before taking the third temperature reading" << endl;
+    }
+
+    // Input third temperature
+    cout << "Enter the third temperature reading: ";
+    cin >> temp3;
+
+    // Check if oil is ready
+    if (temp3 >= 150 && temp3 <= 190) {
+        cout << "You may start frying the Magwinyas." << endl;
+    }
+    else {
+        cout << "Oil is not ready for frying" << endl;
+    }
+
+    return 0;
+}
